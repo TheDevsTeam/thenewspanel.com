319f5053b2983eaed30e0136528bcb21f858364f
Cheikh Ndiongue <Cheikhndiongue3@gmail.com>
2017-11-26 15:11:52 -0500

    fixed the database set up from news to thenewspanel

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,9 @@ set :deploy_to, '/home/deploy/thenewspanel.com'
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
