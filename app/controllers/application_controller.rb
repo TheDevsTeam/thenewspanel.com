591257cb0d5056e6addd79658ed0a1621d197d05
Cheikh Ndiongue <Cheikhndiongue3@gmail.com>
2017-12-03 19:39:36 -0500

    Trying to add seo to rails app. Also change the page_title from All Posts to Global Trending News.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout 'author'
-  
+
+  BRAND_NAME = 'GlobalTrendingNews'.freeze
+
+  def meta_title(title)
+    [title, BRAND_NAME].reject(&:empty?).join(' | ')
+  end
+
 end
--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -11,6 +11,8 @@ module Authors
         @category_id = Category.find_by(name: params[:category]).id
   			@posts = Post.where(category_id: @category_id).order("created_at DESC")
       end
+      @meta_title = meta_title 'The NewsPanel'
+      # @meta_description = ‘Global Trending News’
     end
 
     # GET /posts/1
